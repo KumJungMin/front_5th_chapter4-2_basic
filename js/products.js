312b0b1da69ad9a32b65b88d1368bed924b4faca
KumJungMin <rmawjdals@naver.com>
2025-06-05 20:05:59 +0900

    perf: 요소를 한 번에 추가하기

--- js/products.js
+++ js/products.js
@@ -5,29 +5,23 @@ async function loadProducts() {
 }
 
 function displayProducts(products) {
-
-    // Find the container where products will be displayed
     const container = document.querySelector('#all-products .container');
+    const fragment = document.createDocumentFragment();
 
-   
-    // Iterate over each product and create the HTML structure safely
     products.forEach(product => {
-        // Create the main product div
         const productElement = document.createElement('div');
         productElement.classList.add('product');
 
-        // Create the product picture div
         const pictureDiv = document.createElement('div');
         pictureDiv.classList.add('product-picture');
         const img = document.createElement('img');
         img.src = product.image;
         img.alt = `product: ${product.title}`;
         img.width = 250;
-        img.loading = 'lazy'; 
-        img.style.height = 'auto'; 
+        img.loading = 'lazy';
+        img.style.height = 'auto';
         pictureDiv.appendChild(img);
 
-        // Create the product info div
         const infoDiv = document.createElement('div');
         infoDiv.classList.add('product-info');
 
@@ -48,22 +42,13 @@ function displayProducts(products) {
         const button = document.createElement('button');
         button.textContent = 'Add to bag';
 
-        // Append elements to the product info div
-        infoDiv.appendChild(category);
-        infoDiv.appendChild(title);
-        infoDiv.appendChild(price);
-        infoDiv.appendChild(button);
-
-        // Append picture and info divs to the main product element
-        productElement.appendChild(pictureDiv);
-        productElement.appendChild(infoDiv);
-
-        // Append the new product element to the container
-        container.appendChild(productElement);
+        infoDiv.append(category, title, price, button);
+        productElement.append(pictureDiv, infoDiv);
+        fragment.appendChild(productElement);
     });
 
-    
-
+    // 최종적으로 한 번만 DOM에 추가
+    container.appendChild(fragment);
 }
 
 
